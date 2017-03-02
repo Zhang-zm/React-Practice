2970ada8a2ae7101412c534eea9e8909766cc526
Zhang_zm <763876185@qq.com>
2017-03-02 23:23:04 +0800

    Header 子组件

---
+++ React-Native Demo/textComponent/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+// 定义定义eader 组件
+var Header = React.createClass({
+
+});
+
+// Header 样式
+
+
+//导出组件
+module.exports = Header;
