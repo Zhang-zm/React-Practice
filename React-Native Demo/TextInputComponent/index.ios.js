7ae5054901b96879e9995800b56384c922c05a41
Zhang_zm <763876185@qq.com>
2017-03-08 11:57:24 +0800

    TextInput 组件

--- React-Native Demo/TextInputComponent/index.ios.js
+++ React-Native Demo/TextInputComponent/index.ios.js
@@ -9,45 +9,96 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
-export default class TextInputComponent extends Component {
-  render() {
+/*
+  TextInput 是一个允许用户再应用当中通过键盘输入文本的基本组件
+
+  此组件的属性提供了多种特性的配置，比如自动完成、自动大小写、占位文字，以及多种不同的键盘类型（如纯数字键盘）等。
+
+  常用属性：
+  placeholder
+  value
+  password
+  editable
+  returnKeyType （键盘return 键类型）
+  onChange
+  onEndEditing
+  onSubmitEditing
+  ......
+*/
+
+var TextInputComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      inputText:""
+    };
+  },
+  // 输入框的onChange 处理函数
+  getContent: function(text){
+    this.setState({
+      inputText: text
+    })
+  },
+  clickBtn: function(){
+    alert(this.state.inputText);
+  },
+  render: function(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.flex}>
+          <TextInput
+          style={styles.input}
+          returnKeyType="search"
+          placeholder="请输入内容"
+          onChangeText={this.getContent}/>
+        </View>
+        <TouchableOpacity
+        style={styles.btn}
+        onPress={this.clickBtn}>
+          <Text style={styles.search}>搜索</Text>
+        </TouchableOpacity>
       </View>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+
+var styles = StyleSheet.create({
+  container:{
+    flexDirection:"row",
+    height:45,
+    marginTop:25
+  },
+  flex:{
+    flex:1
+  },
+  input:{
+    height:45,
+    borderWidth:1,
+    marginLeft:5,
+    paddingLeft:5,
+    borderColor:"#ccc",
+    borderRadius:4
+  },
+  btn:{
+    width:55,
+    marginRight:5,
+    marginLeft:5,
+    backgroundColor:"#23beff",
+    height:43,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  search:{
+    color:"#fff",
+    fontSize:15,
+    fontWeight:"bold"
+  }
+});
+
+
 
 AppRegistry.registerComponent('TextInputComponent', () => TextInputComponent);
