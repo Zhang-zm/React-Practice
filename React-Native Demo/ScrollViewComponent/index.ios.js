755ba62edf4720ba9784ccaa89ebb96559978878
Zhang_zm <763876185@qq.com>
2017-03-10 10:07:32 +0800

    ScrollView 组件的基本功能
    有关ScrollView 组件的开始／结束拖拽，开始／结束滑动，刷新等操作的绑定；RefreshControl 的结合使用

---
+++ React-Native Demo/ScrollViewComponent/index.ios.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+/*
+  练习一
+
+  实现ScrollView 的基本功能
+*/
+
+var ScrollViewComponent = require("./myScrollView");
+
+/*
+
+  练习二
+
+  电影列表
+
+*/
+
+
+
+AppRegistry.registerComponent('ScrollViewComponent', () => ScrollViewComponent);
